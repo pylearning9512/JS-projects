eb1a4889590767f927c56e5fffefccb8e344201c
Ethan <pylearning9512@gmail.com>
2025-08-09 04:23:50 -0600

    Add files via upload
    trying out object oriented programming in JS

---
+++ classes.js
@@ -0,0 +1,28 @@
+
+class Shape{
+ constructor(name,size,area){
+    this.size=size;
+    this.name=name;
+    this.area=area;
+ }
+ shapeArea(){
+    console.log(`the area of this ${this.name} is ${this.area} cm.`)
+ }
+}
+
+//instanciating an object from the shape class
+const triangle=new Shape("triangle");
+
+//defining the size by using an object :D
+triangle.size={base:10,height:20}
+//using a template literal/string interpolation to print out the area of the instance
+triangle.area=(triangle.size.base*triangle.size.height)/2
+
+console.log("this is me testing classes in js\n")
+
+console.log(`the name of the shape instance is ${triangle.name}`)
+triangle.shapeArea()
+
+
+
+
