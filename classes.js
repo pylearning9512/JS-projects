68c4f32b1f20b56df7183a167ef0ba6266b60eef
Ethan <pylearning9512@gmail.com>
2025-09-16 07:44:59 -0600

    Update classes.js
    added missing ")"

--- classes.js
+++ classes.js
@@ -15,13 +15,15 @@ const triangle=new Shape("triangle");
 
 //defining the size by using an object :D
 triangle.size={
- base:Number(prompt("what is the base in cm?: ",height:Number(prompt("what is the  height in cm?: "}
+ base:Number(prompt("what is the base in cm?: ")),
+ height:Number(prompt("what is the  height in cm?: "))
+}
+
 //using a template literal/string interpolation to print out the area of the instance
 triangle.area=(triangle.size.base*triangle.size.height)/2
 
 console.log("this is me testing classes in js\n")
 
-alert(`the name of the shape instance is ${triangle.name}`)
 triangle.shapeArea()
 
 
